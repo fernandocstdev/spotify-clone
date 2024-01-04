eb723b869976e260596cfdd02dc18a01b2af36be
fernandocst03 <fernando.dorantescst@gmail.com>
2024-01-04 15:14:37 -0600

    initial commit

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const currentTime = new Date();
+	const currentHour = currentTime.getHours();
+
+	let greeting = "";
+
+	if (currentHour < 12) greeting = "Buenos días";
+	else if (currentHour < 18) greeting = "Buenas tardes";
+	else greeting = "Buenas noches";
+</script>
+
+<h1 class="text-2xl font-bold text-zinc-200">{greeting}</h1>
---
+++ src/icons/Play.jsx
@@ -0,0 +1,7 @@
+export function Play() {
+	return (
+		<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" class="h-5 w-5 text-gray-950" fill="currentColor">
+			<path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path>
+		</svg>
+	)
+}
